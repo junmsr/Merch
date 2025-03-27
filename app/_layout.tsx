b009ce957747fb5f5b043d7bdd2fec6563581f68
junmsr-github <131874822+junmsr-github@users.noreply.github.com>
2025-03-27 16:34:08 +0800

    nav bar was removed/hidden

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,6 +31,11 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="signup" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="dashboard" options={{ headerShown: false }} />
+        <Stack.Screen name="cart" options={{ headerShown: false }} />
+        <Stack.Screen name="profile" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
